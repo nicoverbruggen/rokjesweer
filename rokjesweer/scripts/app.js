d404092b24f771d752ce45582d51cafc4ace4942
Nico Verbruggen <nico.verb@gmail.com>
2014-03-22 10:32:21 +0100

    AngularJS base added

--- rokjesweer/scripts/app.js
+++ rokjesweer/scripts/app.js
@@ -1,3 +1,53 @@
-/**
- * Created by nicoverbruggen on 22/03/14.
- */
+'use strict';
+
+angular.module('LocalStorageModule').value('prefix', 'afg.rokjesweer');
+angular.module('afg.rokjesweer.Controllers', []);
+angular.module('afg.rokjesweer.Directives', []);
+angular.module('afg.rokjesweer.Services', []);
+
+var app = angular
+/*************************************************************************
+ SET UP MODULES & CONFIGURE THEM
+ **************************************************************************/
+    .module(
+        'afg.rokjesweer',
+        [
+            'ngRoute',
+            'ngAnimate',
+            'ui.directives',
+            'afg.rokjesweer.Controllers',
+            'afg.rokjesweer.Directives',
+            'afg.rokjesweer.Services',
+            'LocalStorageModule'
+        ]
+    )
+    //
+    // Configure the modules in question
+    //
+    .config(
+        ['$routeProvider','$locationProvider', '$httpProvider', function($routeProvider, $locationProvider, $httpProvider){
+            // CROSS DOMAIN CALLS OK
+            $httpProvider.defaults.useXDomain = true;
+            // DELETE REQUESTED WITH
+            delete $httpProvider.defaults.headers.common['X-Requested-With'];
+            /**************
+             ROUTES SETUP
+             **************/
+            $routeProvider.when(
+                '/', {
+                    templateUrl:'views/Home.html',
+                    controller:'afg.rokjesweer.Controllers.CtrlHome'
+                });
+
+        }]
+    )
+/*************************************************************************
+ RUN THE APPLICATION
+ **************************************************************************/
+    .run(['$rootScope', '$timeout', '$location', 'localStorageService', '$route',
+        function($rootScope, $timeout, $location, localStorageService, $route){
+
+
+        }
+    ]);
+
